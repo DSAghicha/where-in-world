3fda7321d0395fbdf90db8f151d3a2695b1e2c15
Darshaan Aghicha <dsaghicha211@gmail.com>
2023-07-05 22:17:40 +0530

    Added App

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.scss'
+import React from 'react'
+
+const App: React.FC = () => {
+	return (
+		<>
+			<section className='app'>
+				<p>ok</p>
+				<button>Test</button>
+			</section>
+		</>
+	)
+}
+
+export default App
