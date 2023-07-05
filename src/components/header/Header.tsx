a7e50ba760e3de68e873553a99d4bd288a03d0a2
Darshaan Aghicha <dsaghicha211@gmail.com>
2023-07-05 23:10:05 +0530

    Initial header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ThemeBtn from '../themeBtn/ThemeBtn'
+
+const Header: React.FC = () => {
+	return (
+		<header>
+			<div>Header</div>
+			<ThemeBtn />
+		</header>
+	)
+}
+
+export default Header
