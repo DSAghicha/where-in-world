24668abd3f0b6147482cd93c3aaab01295cff4dc
Darshaan Aghicha <dsaghicha211@gmail.com>
2023-07-08 12:40:12 +0530

    Included Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss'
 import React from 'react'
+import { Header } from './components'
 
 const App: React.FC = () => {
 	return (
 		<>
 			<section className='app'>
+				<Header />
 				<p>ok</p>
 				<button>Test</button>
 			</section>
