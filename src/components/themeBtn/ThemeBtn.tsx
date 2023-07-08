ec8eb86b462246a44677b58931246dba2d47fc04
Darshaan Aghicha <dsaghicha211@gmail.com>
2023-07-08 13:50:07 +0530

    Theme Button Component

--- src/components/themeBtn/ThemeBtn.tsx
+++ src/components/themeBtn/ThemeBtn.tsx
@@ -1,15 +1,34 @@
 import React from 'react'
 import useDarkMode from 'use-dark-mode'
 import { useTheme } from '../../hooks'
+import { DarkModeSvg, LightModeSvg } from '../../assets'
 
 const ThemeBtn: React.FC = () => {
 	const getCurrentTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
 	const darkMode = useDarkMode(getCurrentTheme)
 	const theme = useTheme()
 	return (
-		<button className='btnTheme' onClick={darkMode.toggle}>
-			{theme === 'dark-mode' ? 'LM' : 'DM'}
-		</button>
+		<div className='btnTheme' onClick={darkMode.toggle}>
+			{theme === 'dark-mode' ? <LightButton /> : <DarkButton />}
+		</div>
+	)
+}
+
+const LightButton: React.FC = () => {
+	return (
+		<>
+			<LightModeSvg />
+			<span className='buttonContent'>Light Mode</span>
+		</>
+	)
+}
+
+const DarkButton: React.FC = () => {
+	return (
+		<>
+			<DarkModeSvg />
+			<span className='buttonContent'>Dark Mode</span>
+		</>
 	)
 }
 
